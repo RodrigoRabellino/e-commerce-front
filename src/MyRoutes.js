e701fda7eb47aadd4f2c37e09e7c0804d2d33427
SoleDalia <104218640+SoleDalia@users.noreply.github.com>
2022-06-21 15:21:10 -0300

    merge Ro

--- src/MyRoutes.js
+++ src/MyRoutes.js
@@ -1,12 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import MainPage from "./components/admin/dashboard/DashBoard";
 import HomePage from "./components/homePage/HomePage";
+import MyCarousel from "./components/homePage/MyCarousel";
 
 const MyRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/admin" element={<MainPage />} />
+      <Route path="/myCarousel" element={<MyCarousel />} />
+      {/* <Route path="/productDetail" element={<ProductDetail />} /> */}
     </Routes>
   );
 };
