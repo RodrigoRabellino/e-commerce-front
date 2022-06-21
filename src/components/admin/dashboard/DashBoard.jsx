43cda99a83c8498d595e916a67c3d89151e941c9
Rodrigo Rabellino <30850683+RodrigoRabellino@users.noreply.github.com>
2022-06-21 14:54:52 -0300

    admin dashboard

--- src/components/admin/dashboard/DashBoard.jsx
+++ src/components/admin/dashboard/DashBoard.jsx
@@ -1,12 +1,105 @@
-import { Box, Paper } from "@mui/material";
-import { Grid } from "react-bootstrap";
+import { Box, Paper, Grid, IconButton } from "@mui/material";
+import { MoreVert } from "@mui/icons-material";
+import MyChartBar from "./myCharts/MyChartBar";
 
 const DashBoard = () => {
   return (
-    <Box width="100%" height="100%">
-      <Grid spacing={4}>
-        <Paper sx={{ width: "300px", height: "150px" }}></Paper>
-      </Grid>
+    <Box
+      width="100%"
+      height="100vh"
+      marginTop="65px"
+      display="flex"
+      padding="1rem"
+      alignItems="flex-start"
+      justifyContent="center"
+    >
+      <Box padding="1rem">
+        <Grid container spacing={4}>
+          <Grid item lg="auto">
+            <Paper
+              elevation={0}
+              sx={{ width: "300px", height: "150px", padding: "0.65rem" }}
+            >
+              <Box
+                sx={{
+                  width: "100%",
+                  height: "33%",
+                  display: "flex",
+                  justifyContent: "end",
+                }}
+              >
+                <IconButton>
+                  <MoreVert color="primary" />
+                </IconButton>
+              </Box>
+            </Paper>
+          </Grid>
+          <Grid item lg="auto">
+            <Paper
+              elevation={0}
+              sx={{ width: "300px", height: "150px", padding: "0.65rem" }}
+            >
+              <Box
+                sx={{
+                  width: "100%",
+                  height: "33%",
+                  display: "flex",
+                  justifyContent: "end",
+                }}
+              >
+                <IconButton>
+                  <MoreVert color="primary" />
+                </IconButton>
+              </Box>
+            </Paper>
+          </Grid>
+          <Grid item lg="auto">
+            <Paper
+              elevation={0}
+              sx={{ width: "300px", height: "150px", padding: "0.65rem" }}
+            >
+              <Box
+                sx={{
+                  width: "100%",
+                  height: "33%",
+                  display: "flex",
+                  justifyContent: "end",
+                }}
+              >
+                <IconButton>
+                  <MoreVert color="primary" />
+                </IconButton>
+              </Box>
+            </Paper>
+          </Grid>
+        </Grid>
+        <Box width="100%">
+          <MyChartBar />
+        </Box>
+      </Box>
+      <Paper
+        elevation={0}
+        sx={{
+          width: "300px",
+          height: "100%",
+          padding: "0.65rem",
+          bgcolor: "#bf8832",
+        }}
+      >
+        <Box
+          sx={{
+            width: "100%",
+            height: "33%",
+            display: "flex",
+            justifyContent: "end",
+            alignItems: "start",
+          }}
+        >
+          <IconButton>
+            <MoreVert sx={{ color: "#f2dbb8" }} />
+          </IconButton>
+        </Box>
+      </Paper>
     </Box>
   );
 };
