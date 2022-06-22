923b912969b9b61a3c65587cfbf61041f0f768da
1986SebastianSosa <104211986+1986SebastianSosa@users.noreply.github.com>
2022-06-22 15:49:58 -0300

    solución de conflictos en MyRoutes y Footer

--- src/MyRoutes.js
+++ src/MyRoutes.js
@@ -2,14 +2,11 @@ import { Route, Routes } from "react-router-dom";
 import DashBoard from "./components/admin/dashboard/DashBoard";
 import GridCategories from "./components/gridCategories/GridCategories";
 import HomePage from "./components/homePage/HomePage";
-<<<<<<< Updated upstream
 import ProductDetail from "./components/productDetail/ProductDetail";
 import AboutUs from "./components/aboutUs/AboutUs";
 import StorePage from "./components/storePage/StorePage";
-=======
 import MyCarousel from "./components/homePage/MyCarousel";
-import Login from "./components/admin/login/Login"
->>>>>>> Stashed changes
+import Login from "./components/admin/login/Login";
 
 const MyRoutes = () => {
   return (
@@ -19,11 +16,8 @@ const MyRoutes = () => {
       <Route path="/productDetail" element={<ProductDetail />} />
       <Route path="/aboutUs" element={<AboutUs />} />
       <Route path="/categories" element={<GridCategories />} />
-<<<<<<< Updated upstream
       <Route path="/store" element={<StorePage />} />
-=======
-      <Route path="/login" element={<Login/>} />
->>>>>>> Stashed changes
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 };
