3c4944b88f8642c90ae706973304826bfe81cb57
Juan Arede <85207414+juanarede@users.noreply.github.com>
2022-06-26 21:31:49 -0300

    pequeños cambios en la Home

--- src/components/homePage/HomePage.jsx
+++ src/components/homePage/HomePage.jsx
@@ -1,10 +1,17 @@
-import { Container, Grid, Box, Typography } from "@mui/material";
+import { Container, Grid, Box, Typography, Divider } from "@mui/material";
+
 import React, { useEffect, useState } from "react";
 import { fetchStarredProducts } from "../../services/apiServices";
+<<<<<<< Updated upstream
 import GridCategories from "../gridCategories/GridCategories";
 import ExampleCarousel from "../../components/ExampleCarousel/ExampleCarousel";
 import MyCarousel from "./MyCarousel";
 import { useTheme } from "@emotion/react";
+=======
+import GridCategories from "../../components/gridCategories/GridCategories";
+
+import "./HomePage.css";
+>>>>>>> Stashed changes
 
 const HomePage = () => {
   const [starredProducts, setStarredProducts] = useState([]);
@@ -19,6 +26,7 @@ const HomePage = () => {
   const theme = useTheme();
 
   return (
+<<<<<<< Updated upstream
     <Box
       sx={{
         width: "100vw",
@@ -36,14 +44,31 @@ const HomePage = () => {
           <img
             srcSet={require("../../assets/images/acousticguitar.jpg")}
             style={{
-              width: "100%",
-              height: "100%",
-              objectFit: "cover",
-            }}
-          />
-        </Box>
-      </section>
+=======
+    <>
       <Box>
+        <section>
+          <Box
+            sx={{
+>>>>>>> Stashed changes
+              width: "100%",
+              height: "500px",
+            }}
+          >
+            <img
+              srcSet={require("../../assets/images/acousticguitar.jpg")}
+              style={{
+                width: "100%",
+                height: "100%",
+                objectFit: "cover",
+                opacity: 0.8,
+              }}
+            />
+          </Box>
+        </section>
+      </Box>
+      <Box>
+<<<<<<< Updated upstream
         <Typography
           fontWeight="600"
           marginTop="50px"
@@ -59,11 +84,23 @@ const HomePage = () => {
         >
           Life without playing music is inconceivable for me.
         </Typography>
+=======
+        <Container>
+          <Typography fontWeight="600" marginTop="50px" variant="h4">
+            Music Inspires
+          </Typography>
+          <Typography variant="h6" sx={{ marginTop: "0.4rem" }}>
+            Life without playing music is inconceivable for me.
+          </Typography>
+          <Divider sx={{ marginTop: "0.8rem" }}></Divider>
+        </Container>
+>>>>>>> Stashed changes
       </Box>
 
       <Box sx={{ paddingY: "3rem", marginY: "3rem" }}>
         <GridCategories />
       </Box>
+<<<<<<< Updated upstream
       <Grid
         container
         p={2}
@@ -82,6 +119,23 @@ const HomePage = () => {
         </Grid>
       </Grid>
     </Box>
+=======
+    
+      <section className="parallax">
+        <div className="parallax-inner">
+          <h3 className="title-parallax">Lorem Impsum</h3>
+        </div>
+      </section>
+
+      <Box>
+        <Typography>
+          <h3>Lorem ipsum</h3>
+        </Typography>
+      </Box>
+
+
+    </>
+>>>>>>> Stashed changes
   );
 };
 
