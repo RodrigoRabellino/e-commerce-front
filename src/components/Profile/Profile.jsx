a20197d3f0e58442a13da3c79e905dfa8af9eb6a
Juan Arede <85207414+juanarede@users.noreply.github.com>
2022-07-05 00:01:37 -0300

    pro

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,43 +1,50 @@
-import Box from "@mui/material/Box";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
+import {
+  Divider,
+  Box,
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
 
-
+import avatarimg from "../../assets/images/depositphotos_58414213-stock-photo-acoustic-guitar.jpeg";
+import "./Profile.css";
 
 function Profile() {
   return (
-    <Card sx={{ display: "flex", paddingTop: "5rem" }}>
-      <Box sx={{ display: "flex", flexDirection: "column" }}>
-        <CardContent sx={{ flex: "1 0 auto" }}>
-          <Typography component="div" variant="h5">
-            Live From Space
-          </Typography>
-          <Typography
-            variant="subtitle1"
-            color="text.secondary"
-            component="div"
-          >
-            Mac Miller
-          </Typography>
-        </CardContent>
-        <Box sx={{ display: "flex", alignItems: "center", pl: 1, pb: 1 }}>
-          <IconButton aria-label="previous"></IconButton>
-          <IconButton aria-label="play/pause">
-          
-          </IconButton>
-          <IconButton aria-label="next"></IconButton>
-        </Box>
+    <>
+      <Box sx={{ paddingTop: "10rem", paddingBottom: "10rem" }}>
+        <Card
+          sx={{
+            display: "flex",
+            maxWidth: 450,
+            margin: "0 auto",
+            height: "18rem",
+            backgroundColor: "white",
+          }}
+        >
+          <CardMedia
+            component="img"
+            sx={{ width: 151 }}
+            image={avatarimg}
+            alt="Live from space album cover"
+          />
+          <Box sx={{ display: "flex", flexDirection: "column" }}>
+            <CardContent sx={{ flex: "1 0 auto" }}>
+              <Typography component="div" variant="h5">
+                User Profile
+              </Typography>
+              <Divider></Divider>
+              <Typography
+                variant="subtitle1"
+                color="text.secondary"
+                component="div"
+              ></Typography>
+            </CardContent>
+          </Box>
+        </Card>
       </Box>
-      <CardMedia
-        component="img"
-        sx={{ width: 151 }}
-      
-
-      />
-    </Card>
+    </>
   );
 }
 
